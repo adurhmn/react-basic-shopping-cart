29ee69ce83687aefe46c490e4a60eb883830d0b0
adurhmn <abdurmrahman7@gmail.com>
2022-05-10 23:54:35 +0530

    added bump effect to cart

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Header.module.css";
 
 export default function Header(props) {
   // console.log(props);
+  const [bubbleAdded, setBubbleAdded] = useState(false);
+
+  useEffect(() => {
+    // adds animation class after component is rendered (to see it in every rerender)
+    setBubbleAdded(true);
+
+    // removes the animation class from cart once animation is completed
+    const bubbleTimer = setTimeout(() => {
+      setBubbleAdded(false);
+    }, 300);
+
+    // clean up function
+    return () => {
+      clearInterval(bubbleTimer);
+    };
+  }, [props.cartCount]);
+
+  const cartClass = `${styles.cart} ${bubbleAdded ? styles.bubbleUp : ""}`;
+  console.log(cartClass);
 
   return (
     <header className={styles.header}>
       <span className="logo">AbdurMeals</span>
-      <div className={styles.cart} onClick={props.openModal}>
+      <div className={cartClass} onClick={props.openModal}>
         <span>
           <i className="fa-solid fa-cart-shopping"></i>
         </span>
